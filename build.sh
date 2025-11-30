2c95ef9616a8c740ca269f6ad04d6c28ae771a5c
khuongtrinh2 <khuong.trinh.xg@renesas.com>
2025-11-30 19:56:05 +0700

    android_utilities: Add script to build libGPUInfo
    This script is used to automatically build the libGPUInfo source
    
    Repo:
    https://github.com/ARM-software/libGPUInfo
    
    Signed-off-by: khuongtrinh2 <khuong.trinh.xg@renesas.com>

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export WORKSPACE=`pwd`
+
+cd $WORKSPACE
+
+# Download Android NDK (r27)
+wget https://dl.google.com/android/repository/android-ndk-r27d-linux.zip
+unzip android-ndk-r27d-linux.zip
+
+# Export Android SDK
+export ANDROID_NDK_HOME=${WORKSPACE}/android-ndk-r27d
+
+# Clone libGPUInfo source
+git clone https://github.com/ARM-software/libGPUInfo.git
+
+pushd ${WORKSPACE}/libGPUInfo
+./android_build.sh Release
+popd
